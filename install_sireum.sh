3129b36fa75c1956aa03dc69aa02357900254040
Karl Hoech <karl.hoech@collins.com>
2026-01-14 13:15:09 -0600

    Remove BriefCASE

--- install_sireum.sh
+++ install_sireum.sh
@@ -10,5 +10,5 @@ pushd $SIREUM_DIR/bin
 curl -JLso init.sh https://raw.githubusercontent.com/sireum/kekinian/$SIREUM_V/bin/init.sh
 bash init.sh
 install/fmide.cmd
-#linux/fmide/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -uninstallIU com.collins.trustedsystems.briefcase.feature.feature.group
+linux/fmide/fmide -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -uninstallIU com.collins.trustedsystems.briefcase.feature.feature.group
 popd
