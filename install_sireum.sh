549777d148a4b4e8098c28241c9322a4ec186eff
Karl Hoech <karl.hoech@collins.com>
2026-02-18 06:08:33 -0600

    Remove FMIDE - not needed in this container

--- install_sireum.sh
+++ install_sireum.sh
@@ -9,6 +9,4 @@ mkdir -p $SIREUM_DIR/bin
 pushd $SIREUM_DIR/bin
 curl -JLso init.sh https://raw.githubusercontent.com/sireum/kekinian/$SIREUM_V/bin/init.sh
 bash init.sh
-install/fmide.cmd
-linux/fmide/fmide -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -uninstallIU com.collins.trustedsystems.briefcase.feature.feature.group
 popd
